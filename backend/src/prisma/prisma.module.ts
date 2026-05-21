b9766016a59cf06ed1cc14156a2de9a04fe46d95
dineshctoula <ds9806026041@gmail.com>
2026-05-21 12:40:15 +0545

    contact page backend

---
+++ backend/src/prisma/prisma.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}
+
+
+// yesle reusable primsa module banauxa so other modules can easily access the database services 
---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,11 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
+
+// through prisma service ts . databse can be used anywhere in the project  through dependency injection 
